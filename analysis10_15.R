385e922f2195423a7f7a69d0bf4f6e0bdb4f5dec
Justin Bleich <jbleich89@gmail.com>
2012-10-15 22:30:44 -0400

    forgot to save

--- analysis10_15.R
+++ analysis10_15.R
@@ -3,10 +3,9 @@
 #save(out10,file="tree10_100.rdata")
 #save(out5,file="tree5_100.rdata")
 
-#load("tree5_100.rdata")
-#load("tree10_100.rdata")
+load("tree5_100.rdata")
+load("tree10_100.rdata")
 
-gene=geneNames[1]
 ngenes=100
 
 ##Count number of true TFs
@@ -24,44 +23,36 @@ p_count10
 s_count5
 s_count10
 
+
+par(mgp=c(1.8,.5,0), mar=c(3,3,3,1))
 ##Correlations for Ntrees
 corrList=numeric(ngenes)
 for(i in 1:ngenes){
 corrList[i]=cor(out5[[geneNames[i]]][["var"]],out10[[geneNames[i]]][["var"]])
 }
 summary(corrList)
-hist(corrList,main="Correlations of Incl.Freqs \n for m=5 and m=10",col="grey",breaks=25)
-
-cor(priorMat,priorWeights)
-class(priorMat)
-((x-mean(x)/sd(x))%*%((y-mean(y))/sd(y)))/250
+hist(corrList,main="Correlations of Incl.Freqs \n for m=5 and m=10",col="grey",breaks=25, xlab="Correlation for Iteration")
 
 
 ##Corrs of TF exp and Var incl freq
 cor5=sapply(1:ngenes, function(x) cor(out5[[geneNames[x]]][["var"]],priorMat[x,]))
 cor10=sapply(1:ngenes, function(x) cor(out10[[geneNames[x]]][["var"]],priorMat[x,]))
-hist(cor5,col="grey",breaks=30,main="Correlations for 5 Trees")
-hist(cor10,col="grey",breaks=35,main="Correlations for 10 Trees")
 
+hist(cor5,col="grey",breaks=30,main="TF Exp. and Incl.Freq \n Correlations for 5 Trees",xlab="Corrs.")
+hist(cor10,col="grey",breaks=30,main="TF Exp. and Incl.Freq \nCorrelations for 10 Trees", xlab="Corrs.")
+par(mfrow=c(1,2))
 
 na.rows= which(is.na(cor10)) ##same for 10
 
 
 ##Shane's Plots
-
+par(mfrow=c(1,2))
 #5 Trees
 lik.cor5=numeric(ngenes)
 for(i in 1:ngenes){
   cor.temp=apply(tf.train,2, function(x) cor(gene.train[,i],x))
   lik.cor5[i]=cor(cor.temp,out5[[geneNames[i]]][["var"]])
 }
-lowlim=min(min(lik.cor5),min(cor5,na.rm=T))
-uplim=max(max(lik.cor5),max(cor5,na.rm=T))
-plot(cor5,lik.cor5, xlim=c(-.3,.7), ylim=c(-.3,.7),pch=16,xlab="Prior and Incl. Freq. Corr.",
-     ylab="Corr. of Data Corrs. and Incl. Freq.", main="Likelihood vs. Prior Correlations \n 5 Trees")
-abline(0,1,col="red",lwd=3)
-
-
 ##10 Trees
 lik.cor10=numeric(ngenes)
 for(i in 1:ngenes){
@@ -70,11 +61,35 @@ for(i in 1:ngenes){
 }
 
 
-lowlim=min(min(lik.cor10),min(cor10,na.rm=T))
-uplim=max(max(lik.cor10),max(cor10,na.rm=T))
-plot(cor10,lik.cor10, xlim=c(lowlim,uplim), ylim=c(lowlim,uplim),pch=16,xlab="Prior and Incl. Freq. Corr.",
+lowlim5=min(min(lik.cor5),min(cor5,na.rm=T))
+uplim5=max(max(lik.cor5),max(cor5,na.rm=T))
+lowlim10=min(min(lik.cor10),min(cor10,na.rm=T))
+uplim10=max(max(lik.cor10),max(cor10,na.rm=T))
+low=min(lowlim5,lowlim10); high=max(uplim5,uplim10)
+
+
+##Plot for 5
+plot(cor5,lik.cor5, xlim=c(low,high), ylim=c(low,high),pch=16,xlab="Prior and Incl. Freq. Corr.",
+     ylab="Corr. of Data Corrs. and Incl. Freq.", main="Likelihood vs. Prior Correlations \n 5 Trees")
+abline(0,1,col="red",lwd=3)
+
+
+##Plot for 10
+plot(cor10,lik.cor10, xlim=c(low,high), ylim=c(low,high),pch=16,xlab="Prior and Incl. Freq. Corr.",
      ylab="Corr. of Data Corrs. and Incl. Freq.", main="Likelihood vs. Prior Correlations \n 10 Trees")
 abline(0,1,col="red",lwd=3)
 
 
+##Null Plots
+par(mfrow=c(3,3))
+for(i in 1:9){
+	hist(out5[[geneNames[1]]][["boot_mat"]][,i],col="grey",breaks=25,
+	main="Sample Null Distribution \n 5 Trees",xlab=paste("TF" ,i, "Incl. Freq."))
+}
+
+par(mfrow=c(3,3))
+for(i in 1:9){
+	hist(out10[[geneNames[1]]][["boot_mat"]][,i],col="grey",breaks=25,
+	main="Sample Null Distribution \n 10 Trees",xlab=paste("TF" ,i, "Incl. Freq."))
+}
   
