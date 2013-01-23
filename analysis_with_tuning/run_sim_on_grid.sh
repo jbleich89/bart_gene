1765dc7c6466e0a1fca5c6901f41c99c0a15ec93
Adam Kapelner <kapelner@gmail.com>
2013-01-23 14:06:25 -0500

    sh file for grid round 2

--- analysis_with_tuning/run_sim_on_grid.sh
+++ analysis_with_tuning/run_sim_on_grid.sh
@@ -4,4 +4,4 @@
 #$ -j y
 # This will execute R in our batch mode
 
-R --no-save '--args GENE_START=1 GENE_END=10' analysis_with_tuning/simulation.R
+R --no-save '--args GENE_START=1 GENE_END=2' < analysis_with_tuning/simulation.R
