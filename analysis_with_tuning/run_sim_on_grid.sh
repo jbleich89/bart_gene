694027646ceb0556f2c6d84d13b90c5c78f9393e
Adam Kapelner <kapelner@gmail.com>
2013-01-23 17:00:43 -0500

    SGE bunching for script

--- analysis_with_tuning/run_sim_on_grid.sh
+++ analysis_with_tuning/run_sim_on_grid.sh
@@ -2,5 +2,12 @@
 
 #$ -m e -M kapelner@wharton.upenn.edu
 #$ -j y
+#$ -N tf_discovery_for_gene_expression
+#$ -t 2
 
-R --no-save '--args GENE_START=1 GENE_END=2' < analysis_with_tuning/simulation.R
+let START=$END
+let END=$SGE_TASK_ID*1
+
+echo "starting R task # $SGE_TASK_ID START: $START END: $END"
+
+R --no-save '--args GENE_START=$START GENE_END=$END' < analysis_with_tuning/simulation.R
