8f2d6f87ecbd2bc82633333a0d35f4f8b81a8a08
Adam Kapelner <kapelner@gmail.com>
2013-01-24 14:53:28 -0500

    run just validations round 2

--- analysis_with_tuning/simulation.R
+++ analysis_with_tuning/simulation.R
@@ -6,6 +6,8 @@ source("analysis_with_tuning/simulation_params.R")
 
 #read in arguments supplied by qsub - this will tell use which gene to process
 args = commandArgs(TRUE)
+print(paste("args:", args))
+
 if (length(args) > 0){
 	for (i in 1 : length(args)){
 		eval(parse(text = args[[i]]))
