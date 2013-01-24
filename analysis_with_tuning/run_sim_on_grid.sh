be0499a72a04fe4376f4af3c6897deeec95b30f7
Adam Kapelner <kapelner@gmail.com>
2013-01-24 15:20:43 -0500

    run just validations round 8

--- analysis_with_tuning/run_sim_on_grid.sh
+++ analysis_with_tuning/run_sim_on_grid.sh
@@ -5,6 +5,6 @@
 #$ -t 1-100
 
 echo "starting R for gene # $SGE_TASK_ID"
-export _JAVA_OPTIONS="-Xms128m -Xmx6000m"
+export _JAVA_OPTIONS="-Xms128m -Xmx5000m"
 
 R --no-save --args GENE_NUM=$SGE_TASK_ID < analysis_with_tuning/simulation.R
