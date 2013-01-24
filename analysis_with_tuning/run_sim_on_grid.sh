c4a7b1cddf73f1adca2d266756d4a6315a69964b
Adam Kapelner <kapelner@gmail.com>
2013-01-24 15:28:52 -0500

    run just validations round 9 bigram

--- analysis_with_tuning/run_sim_on_grid.sh
+++ analysis_with_tuning/run_sim_on_grid.sh
@@ -3,8 +3,9 @@
 #$ -j y
 #$ -N tf_discovery_for_gene_expression
 #$ -t 1-100
+#$ -q bigram
 
 echo "starting R for gene # $SGE_TASK_ID"
-export _JAVA_OPTIONS="-Xms128m -Xmx5000m"
+export _JAVA_OPTIONS="-Xms128m -Xmx7000m"
 
 R --no-save --args GENE_NUM=$SGE_TASK_ID < analysis_with_tuning/simulation.R
