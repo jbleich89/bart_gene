005530c335ad8836b07acf51e42e74bbd0003d09
Adam Kapelner <kapelner@gmail.com>
2013-01-26 22:45:41 -0500

    lowered RAM

--- analysis_with_tuning/run_sim_on_grid.sh
+++ analysis_with_tuning/run_sim_on_grid.sh
@@ -6,6 +6,6 @@
 #$ -q intel
 
 echo "starting R for gene # $SGE_TASK_ID"
-export _JAVA_OPTIONS="-Xms128m -Xmx6500m"
+export _JAVA_OPTIONS="-Xms128m -Xmx5000m"
 
 R --no-save --args GENE_NUM=$SGE_TASK_ID < analysis_with_tuning/simulation.R
