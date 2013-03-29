bac4d3313c575cfa7ea0bfdd90be347ad952bb34
Adam Kapelner <kapelner@gmail.com>
2013-03-28 23:03:49 -0400

    RF CV functionality

---
+++ simulations_for_var_selection/rf_cv_validator.R
@@ -0,0 +1,72 @@
+
+rf_cv_var_selection = function(X, y, ntree, rf_alpha, k_folds = 5){
+	n_cv = nrow(X)
+	if (k_folds <= 1 || k_folds > n_cv){
+		stop("The number of folds must be at least 2 and less than or equal to n, use \"Inf\" for leave one out")
+	}
+	
+	
+	if (k_folds == Inf){ #leave-one-out
+		k_folds = n_cv
+	}	
+	
+	holdout_size = round(n_cv / k_folds)
+	split_points = seq(from = 1, to = n_cv, by = holdout_size)[1 : k_folds]
+	
+	L2_err_mat = matrix(NA, nrow = k_folds, ncol = 2)
+	colnames(L2_err_mat) = c("pointwise", "simul")
+	
+	for (k in 1 : k_folds){
+		cat("cv #", k, "\n", sep = "")
+		#find out the indices of the holdout sample
+		holdout_index_i = split_points[k]
+		holdout_index_f = ifelse(k == k_folds, n_cv, split_points[k + 1] - 1)
+		
+		#pull out training and test data
+		training_X_k = X[-c(holdout_index_i : holdout_index_f), ]
+		training_y_k = y[-c(holdout_index_i : holdout_index_f)]
+		test_X_k = X[holdout_index_i : holdout_index_f, ]
+		text_y_k = y[holdout_index_i : holdout_index_f]
+		
+		#do the stuff
+		rf = randomForest(x = as.matrix(training_X_k) , y = training_y_k, ntree = ntree, importance = T)
+		rf_zscore = importance(rf, type=1 ,scale=T)
+		rf_point_vars = which(rf_zscore > qnorm(1 - rf_alpha))
+		rf_simul_vars = which(rf_zscore > qnorm(1 - rf_alpha / p))
+		
+		#now get L2s
+		if (length(rf_point_vars) == 0){
+			L2_err_mat[k, "pointwise"] = sum((text_y_k - mean(training_y_k))^2)
+		} else {
+			rf_pointwise = randomForest(x = as.matrix(training_X_k[, rf_point_vars]), y = training_y_k, ntree = ntree)
+			y_hat = predict(rf_pointwise, newdata = test_X_k)
+			L2_err_mat[k, "pointwise"] = sum((text_y_k - y_hat)^2)
+		}
+		if (length(rf_simul_vars) == 0){
+			L2_err_mat[k, "simul"] = sum((text_y_k - mean(training_y_k))^2)
+		} else {
+			rf_simul = randomForest(x = as.matrix(training_X_k[, rf_simul_vars]), y = training_y_k, ntree = ntree)
+			y_hat = predict(rf_simul, newdata = test_X_k)
+			L2_err_mat[k, "simul"] = sum((text_y_k - y_hat)^2)
+		}
+	}
+	
+	#now extract the lowest oos-L2 to find the "best" method for variable selection
+	L2_err_by_method = colSums(L2_err_mat)
+	min_var_selection_method = colnames(L2_err_mat)[which(L2_err_by_method == min(L2_err_by_method))]
+	
+	#now (finally) do var selection on the entire data and then return the vars from the best method found via cross-validation
+	cat("final", "\n")
+	rf = randomForest(x = X , y = y, ntree = ntree, importance = T)
+	rf_zscore = importance(rf, type=1 ,scale=T)
+	rf_point_vars = which(rf_zscore > qnorm(1 - rf_alpha))
+	rf_simul_vars = which(rf_zscore > qnorm(1 - rf_alpha / p))
+	
+	if (min_var_selection_method == "pointwise"){
+		important_vars_cv = rf_point_vars
+	} else {
+		important_vars_cv = rf_simul_vars
+	}
+	
+	list(best_method = min_var_selection_method, important_vars_cv = important_vars_cv)
+}
--- simulations_for_var_selection/sims_for_bart_var_selection_friedman_mod.R
+++ simulations_for_var_selection/sims_for_bart_var_selection_friedman_mod.R
@@ -2,10 +2,20 @@ library(MASS)
 tryCatch(library(glmnet), error = function(e){install.packages("glmnet")}, finally = library(glmnet))
 tryCatch(library(randomForest), error = function(e){install.packages("glmnet")}, finally = library(randomForest))
 
+
+
 options(error = recover)
 
 LAST_NAME = "kapelner"
 NOT_ON_GRID = length(grep("wharton.upenn.edu", Sys.getenv(c("HOSTNAME")))) == 0
+
+if (NOT_ON_GRID){
+	setwd("C:/Users/Kapelner/workspace/bart_gene/simulations_for_var_selection")
+} else {
+	setwd("../CGMBART_GPL/")
+}
+
+source("rf_cv_validator.R")
 
 if (NOT_ON_GRID){
 	setwd("C:/Users/Kapelner/workspace/CGMBART_GPL/")
@@ -69,11 +79,12 @@ if (NOT_ON_GRID){
 p = param_mat[iter_num, 1]
 sigsq = param_mat[iter_num, 2]
 
-rep_results = array(NA, c(9, 2, num_replicates))
+rep_results = array(NA, c(10, 2, num_replicates))
 
 ######replicate a few times
 for (nr in 1 : num_replicates){
-#generate friedman model data
+	cat("replicate #", nr, "\n")
+	#generate friedman model data
 	X = matrix(rnorm(n * p), ncol = p)
 	X = as.data.frame(X)
 	colnames(X) = seq(1, p)
@@ -83,70 +94,72 @@ for (nr in 1 : num_replicates){
 	
 	
 	#now build bart machine	
-	bart_machine = build_bart_machine(X, y, num_trees = 1, num_burn_in = 2000, run_in_sample = FALSE, verbose = FALSE)
+#	bart_machine = build_bart_machine(X, y, num_trees = 1, num_burn_in = 2000, run_in_sample = FALSE, verbose = FALSE)
 	
 	#do var selection with bart
-	bart_variables_select_obj = var_selection_by_permute_response_three_methods(bart_machine, plot = ifelse(NOT_ON_GRID, TRUE, FALSE))
-	bart_ptwise_vars = sort(as.numeric(bart_variables_select_obj$important_vars_pointwise))
-	bart_simul_max_vars = sort(as.numeric(bart_variables_select_obj$important_vars_simul_max))
-	bart_simul_se_vars = sort(as.numeric(bart_variables_select_obj$important_vars_simul_se))
+#	bart_variables_select_obj = var_selection_by_permute_response_three_methods(bart_machine, plot = ifelse(NOT_ON_GRID, TRUE, FALSE))
+#	bart_ptwise_vars = sort(as.numeric(bart_variables_select_obj$important_vars_pointwise))
+#	bart_simul_max_vars = sort(as.numeric(bart_variables_select_obj$important_vars_simul_max))
+#	bart_simul_se_vars = sort(as.numeric(bart_variables_select_obj$important_vars_simul_se))
 	
 	#do var selection with a CV-min-RMSE
-	bart_cv_vars = var_selection_by_permute_response_cv(bart_machine)$important_vars_cv
+#	bart_cv_vars = var_selection_by_permute_response_cv(bart_machine)$important_vars_cv
 	
 	#do var selection with stepwise
-	if (p < n){
-		step_reg = stepAIC(object = lm(y ~ ., data = X), direction = "backward" , trace = F)
-		stepwise_backward_vars = names(step_reg$coefficients)
-		stepwise_backward_vars = as.numeric(gsub("`", "", stepwise_backward_vars))
-		stepwise_backward_vars = sort(stepwise_backward_vars[!is.na(stepwise_backward_vars)])
-		
-		step_reg = stepAIC(object = lm(y ~ ., data = X), direction = "forward" , trace = F)
-		stepwise_forward_vars = names(step_reg$coefficients)
-		stepwise_forward_vars = as.numeric(gsub("`", "", stepwise_forward_vars))
-		stepwise_forward_vars = sort(stepwise_forward_vars[!is.na(stepwise_forward_vars)])		
-	}
+#	if (p < n){
+#		step_reg = stepAIC(object = lm(y ~ ., data = X), direction = "backward" , trace = F)
+#		stepwise_backward_vars = names(step_reg$coefficients)
+#		stepwise_backward_vars = as.numeric(gsub("`", "", stepwise_backward_vars))
+#		stepwise_backward_vars = sort(stepwise_backward_vars[!is.na(stepwise_backward_vars)])
+#		
+#		step_reg = stepAIC(object = lm(y ~ ., data = X), direction = "forward" , trace = F)
+#		stepwise_forward_vars = names(step_reg$coefficients)
+#		stepwise_forward_vars = as.numeric(gsub("`", "", stepwise_forward_vars))
+#		stepwise_forward_vars = sort(stepwise_forward_vars[!is.na(stepwise_forward_vars)])		
+#	}
   
     ##do var selection with RF
     rf = randomForest(x = X , y = y, ntree = 500 ,importance = T)
     rf_zscore = importance(rf, type=1 ,scale=T)
 	rf_point_vars = which(rf_zscore > qnorm(1 - rf_alpha))
-	rf_simul_vars = which(rf_zscore > qnorm(1 - rf_alpha / p))                  
+	rf_simul_vars = which(rf_zscore > qnorm(1 - rf_alpha / p))
+	rf_cv_vars = rf_cv_var_selection(X, y, 500, rf_alpha)
   
   
 	#do var selection with lasso
-	lasso_matrix_vars = coef(cv.glmnet(as.matrix(X), y, alpha = 1 , nfolds = 5, type.measure = "mse"))
-	lasso_matrix_vars = which(lasso_matrix_vars != 0) - 1
-	lasso_matrix_vars = sort(lasso_matrix_vars[lasso_matrix_vars != 0]) #kill intercept if it exists
+#	lasso_matrix_vars = coef(cv.glmnet(as.matrix(X), y, alpha = 1 , nfolds = 5, type.measure = "mse"))
+#	lasso_matrix_vars = which(lasso_matrix_vars != 0) - 1
+#	lasso_matrix_vars = sort(lasso_matrix_vars[lasso_matrix_vars != 0]) #kill intercept if it exists
 	
 	
 	#### now what did we get right?
-	obj = calc_prec_rec(true_vars, bart_cv_vars)
-	rep_results[1, , nr] = c(obj$precision, obj$recall)
-	obj = calc_prec_rec(true_vars, bart_ptwise_vars)
-	rep_results[2, , nr] = c(obj$precision, obj$recall)
-	obj = calc_prec_rec(true_vars, bart_simul_max_vars)
-	rep_results[3, , nr] = c(obj$precision, obj$recall)
-	obj = calc_prec_rec(true_vars, bart_simul_se_vars)
-	rep_results[4, , nr] = c(obj$precision, obj$recall)
-	if (p < n){
-		obj = calc_prec_rec(true_vars, stepwise_backward_vars)
-		rep_results[5, , nr] = c(obj$precision, obj$recall)
-		obj = calc_prec_rec(true_vars, stepwise_forward_vars)
-		rep_results[6, , nr] = c(obj$precision, obj$recall)		
-	}
-	obj = calc_prec_rec(true_vars, lasso_matrix_vars)
-	rep_results[7, , nr] = c(obj$precision, obj$recall)
+#	obj = calc_prec_rec(true_vars, bart_cv_vars)
+#	rep_results[1, , nr] = c(obj$precision, obj$recall)
+#	obj = calc_prec_rec(true_vars, bart_ptwise_vars)
+#	rep_results[2, , nr] = c(obj$precision, obj$recall)
+#	obj = calc_prec_rec(true_vars, bart_simul_max_vars)
+#	rep_results[3, , nr] = c(obj$precision, obj$recall)
+#	obj = calc_prec_rec(true_vars, bart_simul_se_vars)
+#	rep_results[4, , nr] = c(obj$precision, obj$recall)
+#	if (p < n){
+#		obj = calc_prec_rec(true_vars, stepwise_backward_vars)
+#		rep_results[5, , nr] = c(obj$precision, obj$recall)
+#		obj = calc_prec_rec(true_vars, stepwise_forward_vars)
+#		rep_results[6, , nr] = c(obj$precision, obj$recall)		
+#	}
+#	obj = calc_prec_rec(true_vars, lasso_matrix_vars)
+#	rep_results[7, , nr] = c(obj$precision, obj$recall)
 	obj = calc_prec_rec(true_vars, rf_point_vars)
 	rep_results[8, , nr] = c(obj$precision, obj$recall)
 	obj = calc_prec_rec(true_vars, rf_simul_vars)
 	rep_results[9, , nr] = c(obj$precision, obj$recall)
-  
+	obj = calc_prec_rec(true_vars, rf_cv_vars)
+	rep_results[10, , nr] = c(obj$precision, obj$recall)  
 	
 }
 
-results = matrix(0, nrow = 9, ncol = 2)
-rownames(results) = c("BART_CV", "BART_pointwise", "BART_simul_max", "BART_simul_se", "stepwise_backward",  "stepwise_forward", "lasso", "RF_point", "RF_simul") 
+results = matrix(0, nrow = 10, ncol = 2)
+rownames(results) = c("BART_CV", "BART_pointwise", "BART_simul_max", "BART_simul_se", "stepwise_backward",  "stepwise_forward", "lasso", "RF_point", "RF_simul", "RF_CV") 
 colnames(results) = c("precision", "recall")
 
 #now dump results in
@@ -160,7 +173,7 @@ F1s = 2 * results[, 1] * results[, 2] / (results[, 1] + results[, 2])
 results = cbind(results, F1s)
 
 #save results
-write.csv(results, file = paste("../bart_gene/simulations_for_var_selection/sim_results/var_sel_sim_friedman_p", p, "_sigsq_", sigsq, ".csv", sep = ""))	
+write.csv(results, file = paste("../bart_gene/simulations_for_var_selection/sim_results/rf_var_sel_sim_friedman_p", p, "_sigsq_", sigsq, ".csv", sep = ""))	
 
 
 ##load results and print them to xtable
