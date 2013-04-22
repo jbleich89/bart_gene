fbc9f687be9f357f2a725dcd75ac859273b37a6c
Adam Kapelner <kapelner@gmail.com>
2013-04-21 20:49:04 -0400

    update on analyzing simulation results

--- simulations_for_var_selection/analyze_sim_results.R
+++ simulations_for_var_selection/analyze_sim_results.R
@@ -7,10 +7,14 @@ model_colors = c(
 	rgb(0.3,0.3,1),		#BART ptwise
 	rgb(0.5,0.5,1),		#BART simul max
 	rgb(0.7,0.7,1),		#BART simul se
-	rgb(0.1,0.1,0.2),	#BART CV with prior
-	rgb(0.3,0.3,0.4),	#BART ptwise with prior
-	rgb(0.5,0.5,0.6),	#BART simul max with prior
-	rgb(0.7,0.7,0.8),		#BART simul se with prior
+	rgb(0.1,0.1,0.2),	#BART CV with GOOD prior
+	rgb(0.3,0.3,0.4),	#BART ptwise with GOOD prior
+	rgb(0.5,0.5,0.6),	#BART simul max with GOOD prior
+	rgb(0.7,0.7,0.8),	#BART simul se with GOOD prior
+	rgb(0.3,0.15,0),	#BART CV with BAD prior
+	rgb(0.5,0.25,0.0),	#BART ptwise with BAD prior
+	rgb(0.7,0.35,0.0),	#BART simul max with BAD prior
+	rgb(0.9,0.45,0),		#BART simul se with BAD prior
 	rgb(0.9,0.9,0),		#Stepwise B
 	rgb(0.9,0.8,0.4),	#Stepwise F
 	rgb(1,0.5,0.5),		#Lasso
@@ -25,7 +29,7 @@ num_models = length(model_colors)
 ##friedman analysis
 ##friedman analysis
 ps = c(25, 100, 200, 500, 1000)
-sigsqs = c(1, 100)
+sigsqs = c(1, 100, 625)
 
 
 spacing = 2
@@ -38,8 +42,9 @@ for (p in ps){
 	names(results)[seq(from = 7, to = length(sigsqs) * num_models + spacing * (length(sigsqs) - 1), by = length(model_colors))] = sigsqs
 	iter = 1
 	for (sigsq in sigsqs){
-		X = read.csv(paste("full_var_sel_sim_friedman_p", p, "_sigsq_", sigsq, ".csv", sep = ""))
+		X = read.csv(paste("complete_var_sel_sim_friedman_p", p, "_sigsq_", sigsq, ".csv", sep = ""))
 		F1_results = X[, 4]
+		F1_results[c(2,3,4,6,7,8,10,11,12,17,18)] = 0
 		results[iter : (iter + num_models - 1)] = F1_results
 		iter = iter + num_models + spacing
 	}
@@ -80,11 +85,12 @@ for (p in ps){
 		for (sigsq in sigsqs){
 			master_iter = master_iter + 1
 			print(master_iter)
-			if (master_iter %in% c(24, 32, 44, 54)){next}
-			X = read.csv(paste("full_var_sel_sim_linear_p", p, "_p0_", p0, "_sigsq_", sigsq, ".csv", sep = ""))
+			X = read.csv(paste("complete_var_sel_sim_linear_p", p, "_p0_", p0, "_sigsq_", sigsq, ".csv", sep = ""))
 			F1_results = X[, 4]
+			F1_results[c(2,3,4,6,7,8,10,11,12,17,18)] = 0
 			results[iter : (iter + num_models - 1)] = F1_results
 			iter = iter + num_models + spacing
+		
 		}
 		barplot(results, 
 				xlab = "sigsq + method", 
@@ -103,4 +109,4 @@ for (p in ps){
 }
 
 
-setwd("C:/Users/Kapelner/Desktop/Dropbox/BART_gene/writeup_4_3_13")
+setwd("C:/Users/Kapelner/Desktop/Dropbox/BART_gene/writeup_4_23_13")
