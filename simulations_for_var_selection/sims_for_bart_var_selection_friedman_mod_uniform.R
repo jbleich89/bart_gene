fc362a971767680cda6502e3e572b5f3d305c30a
Adam Kapelner <kapelner@gmail.com>
2014-02-19 14:09:15 -0500

    runs

--- simulations_for_var_selection/sims_for_bart_var_selection_friedman_mod_uniform.R
+++ simulations_for_var_selection/sims_for_bart_var_selection_friedman_mod_uniform.R
@@ -85,7 +85,7 @@ sigsq = param_mat[iter_num, 2]
 rep_results = array(NA, c(NUM_ALGOS, 2, num_replicates))
 
 ######replicate a few times
-for (nr in 1 : num_replicates){
+for (nr in 24 : num_replicates){
   cat("replicate #", nr, "\n")
   #generate friedman model data
   X = matrix(runif(n * p), ncol = p)
