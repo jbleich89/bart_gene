e00082efed663711057cf291d06c957add670da1
jbleich <jbleich89@gmail.com>
2014-02-23 17:48:06 -0500

    gmax

--- simulations_for_var_selection/sims_for_bart_var_selection_friedman_mod_uniform.R
+++ simulations_for_var_selection/sims_for_bart_var_selection_friedman_mod_uniform.R
@@ -98,12 +98,12 @@ for (nr in 30 : num_replicates){
   #do var selection with bart WITHOUT PRIOR
   bart_machine = build_bart_machine(X, y, num_trees = 1, num_burn_in = 2000, run_in_sample = FALSE, verbose = FALSE)	
   #now build bart machine WITHOUT PRIOR
-  bart_variables_select_obj = var_selection_by_permute_response_three_methods(bart_machine, plot = FALSE)
-  bart_ptwise_vars = sort(as.numeric(bart_variables_select_obj$important_vars_local_names))
+#   bart_variables_select_obj = var_selection_by_permute_response_three_methods(bart_machine, plot = FALSE)
+#   bart_ptwise_vars = sort(as.numeric(bart_variables_select_obj$important_vars_local_names))
   bart_simul_max_vars = sort(as.numeric(bart_variables_select_obj$important_vars_global_max_names))
-  bart_simul_se_vars = sort(as.numeric(bart_variables_select_obj$important_vars_global_se_names))	
+#   bart_simul_se_vars = sort(as.numeric(bart_variables_select_obj$important_vars_global_se_names))	
   #do var selection with a CV-min-RMSE
-  bart_cv_vars = as.numeric(var_selection_by_permute_response_cv(bart_machine)$important_vars_cv)
+#   bart_cv_vars = as.numeric(var_selection_by_permute_response_cv(bart_machine)$important_vars_cv)
   destroy_bart_machine(bart_machine)
   
   #now build bart machine	WITH GOOD PRIOR
@@ -115,11 +115,11 @@ for (nr in 30 : num_replicates){
                                     verbose = FALSE)	
   #do var selection with bart WITH  PRIOR
   bart_variables_select_obj = var_selection_by_permute_response_three_methods(bart_machine, plot = FALSE)
-  bart_ptwise_vars_good_prior = sort(as.numeric(bart_variables_select_obj$important_vars_local_names))
+#   bart_ptwise_vars_good_prior = sort(as.numeric(bart_variables_select_obj$important_vars_local_names))
   bart_simul_max_vars_good_prior = sort(as.numeric(bart_variables_select_obj$important_vars_global_max_names))
-  bart_simul_se_vars_good_prior = sort(as.numeric(bart_variables_select_obj$important_vars_global_se_names))	
+#   bart_simul_se_vars_good_prior = sort(as.numeric(bart_variables_select_obj$important_vars_global_se_names))	
   #do var selection with a CV-min-RMSE
-  bart_cv_vars_good_prior = as.numeric(var_selection_by_permute_response_cv(bart_machine)$important_vars_cv)
+#   bart_cv_vars_good_prior = as.numeric(var_selection_by_permute_response_cv(bart_machine)$important_vars_cv)
   destroy_bart_machine(bart_machine)
   
   
@@ -131,105 +131,106 @@ for (nr in 30 : num_replicates){
                                     run_in_sample = FALSE, 
                                     verbose = FALSE)	
   #do var selection with bart WITH PRIOR
-  bart_variables_select_obj = var_selection_by_permute_response_three_methods(bart_machine, plot = FALSE)
-  bart_ptwise_vars_bad_prior = sort(as.numeric(bart_variables_select_obj$important_vars_local_names))
+   bart_variables_select_obj = var_selection_by_permute_response_three_methods(bart_machine, plot = FALSE)
+#   bart_ptwise_vars_bad_prior = sort(as.numeric(bart_variables_select_obj$important_vars_local_names))
   bart_simul_max_vars_bad_prior = sort(as.numeric(bart_variables_select_obj$important_vars_global_max_names))
-  bart_simul_se_vars_bad_prior = sort(as.numeric(bart_variables_select_obj$important_vars_global_se_names))	
+#   bart_simul_se_vars_bad_prior = sort(as.numeric(bart_variables_select_obj$important_vars_global_se_names))	
   #do var selection with a CV-min-RMSE
-  bart_cv_vars_bad_prior = as.numeric(var_selection_by_permute_response_cv(bart_machine)$important_vars_cv)
+#   bart_cv_vars_bad_prior = as.numeric(var_selection_by_permute_response_cv(bart_machine)$important_vars_cv)
   destroy_bart_machine(bart_machine)
   
   
-  #do var selection with stepwise
-  if (p < n){
-    step_reg = stepAIC(object = lm(y ~ ., data = X), direction = "backward" , trace = F)
-    stepwise_backward_vars = names(step_reg$coefficients)
-    stepwise_backward_vars = as.numeric(gsub("`", "", stepwise_backward_vars))
-    stepwise_backward_vars = sort(stepwise_backward_vars[!is.na(stepwise_backward_vars)])
-    
-    step_reg = stepAIC(object = lm(y ~ ., data = X), direction = "forward" , trace = F)
-    stepwise_forward_vars = names(step_reg$coefficients)
-    stepwise_forward_vars = as.numeric(gsub("`", "", stepwise_forward_vars))
-    stepwise_forward_vars = sort(stepwise_forward_vars[!is.na(stepwise_forward_vars)])		
-  }
-  
-  ##do var selection with RF
-  rf = randomForest(x = X , y = y, ntree = 500 ,importance = T)
-  rf_zscore = importance(rf, type=1 ,scale=T)
-  rf_point_vars = which(rf_zscore > qnorm(1 - rf_alpha))
-  rf_simul_vars = which(rf_zscore > qnorm(1 - rf_alpha / p))
-  rf_cv_vars = rf_cv_var_selection(X, y, 500, rf_alpha)$important_vars_cv
-  
-  
-  #do var selection with lasso
-  lasso_matrix_vars = coef(cv.glmnet(as.matrix(X), y, alpha = 1 , nfolds = 5, type.measure = "mse"))
-  lasso_matrix_vars = which(lasso_matrix_vars != 0) - 1
-  lasso_matrix_vars = sort(lasso_matrix_vars[lasso_matrix_vars != 0]) #kill intercept if it exists
-  
-  ##do var selection with dynaTree
-  dynatree_vars = as.numeric(var_sel_dynaTree(as.matrix(X), y, n_particles = 2500))
-  
-  ##do var selection with spikeslab
-  spikeslab_mod = spikeslab(x = as.matrix(X), y = y, verbose = F, 
-                            bigp.smalln = ifelse(ncol(X) > nrow(X), T, F), max.var = 100)
-  spikeslab_vars = which(spikeslab_mod$gnet > 0)
-  
-  
+#   #do var selection with stepwise
+#   if (p < n){
+#     step_reg = stepAIC(object = lm(y ~ ., data = X), direction = "backward" , trace = F)
+#     stepwise_backward_vars = names(step_reg$coefficients)
+#     stepwise_backward_vars = as.numeric(gsub("`", "", stepwise_backward_vars))
+#     stepwise_backward_vars = sort(stepwise_backward_vars[!is.na(stepwise_backward_vars)])
+#     
+#     step_reg = stepAIC(object = lm(y ~ ., data = X), direction = "forward" , trace = F)
+#     stepwise_forward_vars = names(step_reg$coefficients)
+#     stepwise_forward_vars = as.numeric(gsub("`", "", stepwise_forward_vars))
+#     stepwise_forward_vars = sort(stepwise_forward_vars[!is.na(stepwise_forward_vars)])		
+#   }
+#   
+#   ##do var selection with RF
+#   rf = randomForest(x = X , y = y, ntree = 500 ,importance = T)
+#   rf_zscore = importance(rf, type=1 ,scale=T)
+#   rf_point_vars = which(rf_zscore > qnorm(1 - rf_alpha))
+#   rf_simul_vars = which(rf_zscore > qnorm(1 - rf_alpha / p))
+#   rf_cv_vars = rf_cv_var_selection(X, y, 500, rf_alpha)$important_vars_cv
+#   
+#   
+#   #do var selection with lasso
+#   lasso_matrix_vars = coef(cv.glmnet(as.matrix(X), y, alpha = 1 , nfolds = 5, type.measure = "mse"))
+#   lasso_matrix_vars = which(lasso_matrix_vars != 0) - 1
+#   lasso_matrix_vars = sort(lasso_matrix_vars[lasso_matrix_vars != 0]) #kill intercept if it exists
+#   
+#   ##do var selection with dynaTree
+#   dynatree_vars = as.numeric(var_sel_dynaTree(as.matrix(X), y, n_particles = 2500))
+#   
+#   ##do var selection with spikeslab
+#   spikeslab_mod = spikeslab(x = as.matrix(X), y = y, verbose = F, 
+#                             bigp.smalln = ifelse(ncol(X) > nrow(X), T, F), max.var = 100)
+#   spikeslab_vars = which(spikeslab_mod$gnet > 0)
+#   
+#   
   #### now what did we get right?
   #BART vanilla
-  obj = calc_prec_rec(true_vars, bart_cv_vars)
-  rep_results[1, , nr] = c(obj$precision, obj$recall)
-  obj = calc_prec_rec(true_vars, bart_ptwise_vars)
-  rep_results[2, , nr] = c(obj$precision, obj$recall)
+#   obj = calc_prec_rec(true_vars, bart_cv_vars)
+#   rep_results[1, , nr] = c(obj$precision, obj$recall)
+#   obj = calc_prec_rec(true_vars, bart_ptwise_vars)
+#   rep_results[2, , nr] = c(obj$precision, obj$recall)
   obj = calc_prec_rec(true_vars, bart_simul_max_vars)
   rep_results[3, , nr] = c(obj$precision, obj$recall)
-  obj = calc_prec_rec(true_vars, bart_simul_se_vars)
-  rep_results[4, , nr] = c(obj$precision, obj$recall)
+#   obj = calc_prec_rec(true_vars, bart_simul_se_vars)
+#   rep_results[4, , nr] = c(obj$precision, obj$recall)
   #BART with good prior
-  obj = calc_prec_rec(true_vars, bart_cv_vars_good_prior)
-  rep_results[5, , nr] = c(obj$precision, obj$recall)
-  obj = calc_prec_rec(true_vars, bart_ptwise_vars_good_prior)
-  rep_results[6, , nr] = c(obj$precision, obj$recall)
+#   obj = calc_prec_rec(true_vars, bart_cv_vars_good_prior)
+#   rep_results[5, , nr] = c(obj$precision, obj$recall)
+#   obj = calc_prec_rec(true_vars, bart_ptwise_vars_good_prior)
+#   rep_results[6, , nr] = c(obj$precision, obj$recall)
   obj = calc_prec_rec(true_vars, bart_simul_max_vars_good_prior)
   rep_results[7, , nr] = c(obj$precision, obj$recall)
-  obj = calc_prec_rec(true_vars, bart_simul_se_vars_good_prior)
-  rep_results[8, , nr] = c(obj$precision, obj$recall)	
+#   obj = calc_prec_rec(true_vars, bart_simul_se_vars_good_prior)
+#   rep_results[8, , nr] = c(obj$precision, obj$recall)	
   #BART with bad prior
-  obj = calc_prec_rec(true_vars, bart_cv_vars_bad_prior)
-  rep_results[9, , nr] = c(obj$precision, obj$recall)
-  obj = calc_prec_rec(true_vars, bart_ptwise_vars_bad_prior)
-  rep_results[10, , nr] = c(obj$precision, obj$recall)
+#   obj = calc_prec_rec(true_vars, bart_cv_vars_bad_prior)
+#   rep_results[9, , nr] = c(obj$precision, obj$recall)
+#   obj = calc_prec_rec(true_vars, bart_ptwise_vars_bad_prior)
+#   rep_results[10, , nr] = c(obj$precision, obj$recall)
   obj = calc_prec_rec(true_vars, bart_simul_max_vars_bad_prior)
   rep_results[11, , nr] = c(obj$precision, obj$recall)
-  obj = calc_prec_rec(true_vars, bart_simul_se_vars_bad_prior)
-  rep_results[12, , nr] = c(obj$precision, obj$recall)	
+#   obj = calc_prec_rec(true_vars, bart_simul_se_vars_bad_prior)
+#   rep_results[12, , nr] = c(obj$precision, obj$recall)	
   #the stepwises
-  if (p < n){
-    obj = calc_prec_rec(true_vars, stepwise_backward_vars)
-    rep_results[13, , nr] = c(obj$precision, obj$recall)
-    obj = calc_prec_rec(true_vars, stepwise_forward_vars)
-    rep_results[14, , nr] = c(obj$precision, obj$recall)		
-  }
-  #LASSO
-  obj = calc_prec_rec(true_vars, lasso_matrix_vars)
-  rep_results[15, , nr] = c(obj$precision, obj$recall)
-  #The RF methods
-  obj = calc_prec_rec(true_vars, rf_cv_vars)
-  rep_results[16, , nr] = c(obj$precision, obj$recall) 	
-  obj = calc_prec_rec(true_vars, rf_point_vars)
-  rep_results[17, , nr] = c(obj$precision, obj$recall)
-  obj = calc_prec_rec(true_vars, rf_simul_vars)
-  rep_results[18, , nr] = c(obj$precision, obj$recall)
+#   if (p < n){
+#     obj = calc_prec_rec(true_vars, stepwise_backward_vars)
+#     rep_results[13, , nr] = c(obj$precision, obj$recall)
+#     obj = calc_prec_rec(true_vars, stepwise_forward_vars)
+#     rep_results[14, , nr] = c(obj$precision, obj$recall)		
+#   }
+#   #LASSO
+#   obj = calc_prec_rec(true_vars, lasso_matrix_vars)
+#   rep_results[15, , nr] = c(obj$precision, obj$recall)
+#   #The RF methods
+#   obj = calc_prec_rec(true_vars, rf_cv_vars)
+#   rep_results[16, , nr] = c(obj$precision, obj$recall) 	
+#   obj = calc_prec_rec(true_vars, rf_point_vars)
+#   rep_results[17, , nr] = c(obj$precision, obj$recall)
+#   obj = calc_prec_rec(true_vars, rf_simul_vars)
+#   rep_results[18, , nr] = c(obj$precision, obj$recall)
+#   
+#   ##dynatree
+#   obj = calc_prec_rec(true_vars, dynatree_vars)
+#   rep_results[19, , nr] = c(obj$precision, obj$recall)
+#   
+#   ##spikeslab 
+#   obj = calc_prec_rec(true_vars, spikeslab_vars)
+#   rep_results[20, , nr] = c(obj$precision, obj$recall)  
   
-  ##dynatree
-  obj = calc_prec_rec(true_vars, dynatree_vars)
-  rep_results[19, , nr] = c(obj$precision, obj$recall)
-  
-  ##spikeslab 
-  obj = calc_prec_rec(true_vars, spikeslab_vars)
-  rep_results[20, , nr] = c(obj$precision, obj$recall)  
-  
-  write.csv(rep_results[, , nr], file = paste("partial_var_sel_sim_friedman_uniform_p", p, "_sigsq_", sigsq, "_nr_", nr, "_revised.csv", sep = ""))  
+#   write.csv(rep_results[, , nr], file = paste("partial_var_sel_sim_friedman_uniform_p", p, "_sigsq_", sigsq, "_nr_", nr, "_revised.csv", sep = ""))  
+  write.csv(rep_results[, , nr], file = paste("partial_var_sel_sim_friedman_uniform_p", p, "_sigsq_", sigsq, "_nr_", nr, "_bart_gmax.csv", sep = ""))  
   
   
 }
